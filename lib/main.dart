f89e9a99187188852b1b01fbd04a0b1579e79c6a
Reyhan Ariq Syahalam <ariq.syahalam@gmail.com>
2021-12-16 19:51:21 +0700

    Initiate main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:slowlabapp/routes.dart';
+import './screens/home/home.dart';
 
-void main() {
-  runApp(MaterialApp(
-    onGenerateRoute: RouteGenerator.generateRoute,
-  ));
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SlowLab',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
