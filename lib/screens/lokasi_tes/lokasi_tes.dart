287a345ae7fbe82d6e58a8daf98d448317b49bb5
cynthiaphlndr <cynthia.philander21@gmail.com>
2021-12-31 08:05:08 +0700

    finishing flutter

--- lib/screens/lokasi_tes/lokasi_tes.dart
+++ lib/screens/lokasi_tes/lokasi_tes.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'tambah_lokasi.dart';
+
+import 'dart:core';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class LokasiTesPage extends StatefulWidget {
   const LokasiTesPage({Key? key}) : super(key: key);
@@ -7,12 +13,166 @@ class LokasiTesPage extends StatefulWidget {
 }
 
 class _LokasiTesPageState extends State<LokasiTesPage> {
+  final formKey = GlobalKey<FormState>();
+  final _lokasiController = TextEditingController();
+  List<dynamic> hasilSearch = [];
+
+  void fetchLokasi() async {
+    var result = await http.get(
+        Uri.parse("https://slowlab-core.herokuapp.com/lokasi/get-lokasi/"));
+    var hasilDecode = json.decode(result.body);
+    hasilSearch.clear();
+
+    for (int i = 0; i < hasilDecode.length; i++) {
+      var fields = hasilDecode[i]['fields'];
+
+      var lokasi = fields['provinsi'].toLowerCase();
+      var lokasiControl = _lokasiController.text.toLowerCase();
+      if (lokasi.contains(lokasiControl)) {
+        setState(() {
+          hasilSearch.add(fields);
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("LokasiTes"),
+      appBar: AppBar(
+        title: const Text("Lokasi Tes"),
+      ),
+      body: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              judulDisplay(),
+              inputBox(),
+              tombol(),
+              kartu(),
+              Container(
+                  alignment: Alignment.center,
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(100, 10, 150, 10),
+                  child: ElevatedButton(
+                      child: const Text('Tambah Lokasi'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => TambahLokasi()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.amber,
+                          textStyle: const TextStyle(
+                            color: Colors.indigo,
+                          )))),
+            ],
+          )),
+    );
+  }
+
+  Widget judulDisplay() => Container(
+        alignment: Alignment.center,
+        child: const Text("Cari Lokasi Tes Covid-19",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            )),
+      );
+
+  Widget inputBox() => Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.fromLTRB(100, 10, 150, 10),
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Masukkan Provinsi Lokasi',
+            border: OutlineInputBorder(),
+            contentPadding:
+                EdgeInsets.symmetric(vertical: 25.0, horizontal: 10.0),
+          ),
+          validator: (value) {
+            if (value!.isEmpty) {
+              return 'Enter anything';
+            } else {
+              return null;
+            }
+          },
+          controller: _lokasiController,
+          maxLength: 30,
         ),
-        body: Center(child: Text("Lokasi Tes Page")));
+      );
+
+  Widget tombol() => Container(
+      alignment: Alignment.center,
+      height: 50,
+      padding: const EdgeInsets.fromLTRB(100, 10, 150, 10),
+      child: ElevatedButton(
+          child: const Text('Cari'),
+          onPressed: () {
+            fetchLokasi();
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.indigo,
+              textStyle: const TextStyle(
+                color: Colors.white,
+              ))));
+
+  Widget kartu() => ListView.builder(
+      shrinkWrap: true,
+      itemCount: hasilSearch.length,
+      itemBuilder: (BuildContext context, int index) {
+        return buildCard(hasilSearch[index]);
+      });
+
+  Widget buildCard(data) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Card(
+              child: Container(
+            color: Colors.indigo,
+            padding: const EdgeInsets.fromLTRB(0, 30, 0, 10),
+            alignment: Alignment.center,
+            width: 200,
+            child: Column(
+              children: [
+                Text(data['nama'],
+                    style: TextStyle(
+                      backgroundColor: Colors.amber,
+                      color: Colors.indigo,
+                    )),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(7, 20, 7, 10),
+                    alignment: Alignment.centerLeft,
+                    child: Text('Alamat : ${data['alamat']}',
+                        style: TextStyle(
+                          height: 1,
+                          color: Colors.white,
+                        ))),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(7, 20, 7, 10),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                        'Jam Operasional : ${data['jam_buka']} - ${data['jam_tutup']}',
+                        style: TextStyle(
+                          height: 1,
+                          color: Colors.white,
+                        ))),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(7, 20, 7, 10),
+                    alignment: Alignment.centerLeft,
+                    child: Text('No. Telepon : ${data['nomor']}',
+                        style: TextStyle(
+                          height: 1,
+                          color: Colors.white,
+                        ))),
+              ],
+            ),
+          )),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/screens/lokasi_tes/tambah_lokasi.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:core';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class TambahLokasi extends StatefulWidget {
+  const TambahLokasi({Key? key}) : super(key: key);
+  @override
+  State<TambahLokasi>  createState() => _TambahLokasi();
+}
+
+class _TambahLokasi extends State<TambahLokasi> {
+  final formKey = GlobalKey<FormState>();
+  String namaLokasi = '';
+  String provinsiLokasi = '';
+  String alamatLokasi = '';
+  String nomor = '';
+
+  static List<String> hourList = List<String>.generate(24, (int index) {
+    if (index < 10){
+      return '0$index.00';}
+    else{
+      return '$index.00';}
+  });
+  List<DropdownMenuItem> hourItemList = hourList
+      .map((val) => DropdownMenuItem(value: val, child: Text(val)))
+      .toList();
+  String jamBuka = hourList[0];
+  String jamTutup = hourList[0];
+  bool flag = false;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text("Tambah Lokasi"),
+          backgroundColor: Colors.indigo,
+        ),
+        body: Form(
+          key: formKey,
+          child: ListView(
+            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+            children: [
+              inputNama(),
+              const SizedBox(height: 16),
+              inputProvinsi(),
+              const SizedBox(height: 16),
+              inputAlamat(),
+              const SizedBox(height: 16),
+              inputJamBuka(),
+              inputJamTutup(),
+              inputNomor(),
+              const SizedBox(height: 16),
+              inputTombol(),
+            ],
+          ),
+        ),
+      );
+  Widget inputNama() => TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Nama Lokasi',
+          border: OutlineInputBorder(),
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Enter anything';
+          } else {
+            return null;
+          }
+        },
+        maxLength: 30,
+        onSaved: (value) => setState(() => namaLokasi = value!),
+      );
+
+  Widget inputProvinsi() => TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Provinsi Lokasi',
+          border: OutlineInputBorder(),
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Enter anything';
+          } else {
+            return null;
+          }
+        },
+        maxLength: 30,
+        onSaved: (value) => setState(() => provinsiLokasi = value!),
+      );
+
+  Widget inputAlamat() => TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Alamat Lokasi',
+          border: OutlineInputBorder(),
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Enter anything';
+          } else {
+            return null;
+          }
+        },
+        maxLength: 200,
+        onSaved: (value) => setState(() => alamatLokasi = value!),
+      );
+
+  Widget inputJamBuka() => InputDecorator(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        counter: Container(
+          height: 10,
+        ),
+      ),
+      child: DropdownButton<dynamic>(
+        value: jamBuka,
+        onChanged: (val) => setState(() => jamBuka = (val.toString())),
+        items: hourItemList,
+      ));
+
+  Widget inputJamTutup() => InputDecorator(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        counter: Container(
+          height: 10,
+        ),
+      ),
+      child: DropdownButton<dynamic>(
+        value: jamTutup,
+        onChanged: (val) => setState(() => jamTutup = (val.toString())),
+        items: hourItemList,
+      ));
+
+  Widget inputNomor() => TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Nomor Lokasi',
+          border: OutlineInputBorder(),
+        ),
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly
+        ],
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Enter anything';
+          } else {
+            return null;
+          }
+        },
+        maxLength: 15,
+        onSaved: (value) => setState(() => nomor = value!),
+      );
+
+  Widget inputTombol() => ElevatedButton(
+      child: Text("Tambah Lokasi"),
+      onPressed: () async {
+        final isValid = formKey.currentState!.validate();
+        if (isValid) {
+          formKey.currentState!.save();
+        }
+        {
+          const url = "https://slowlab-core.herokuapp.com/lokasi/add_data/";
+          final response = await http.post(Uri.parse(url),
+              headers: <String, String>{
+                'Content-Type': 'application/json; charset=UTF-8',
+              },
+              body: jsonEncode(<String, String>{
+                'nama': namaLokasi,
+                'provinsi': provinsiLokasi,
+                'alamat': alamatLokasi,
+                'jam_buka': jamBuka,
+                'jam_tutup': jamTutup,
+                'nomor': nomor,
+              }));
+          Map<String, dynamic> tes = jsonDecode(response.body);
+          if (tes['hasil'] == 'lokasi gagal') {
+            showDialog(context: context, builder: (BuildContext context){
+            return AlertDialog(
+              title: Text("Gagal menambahkan lokasi"),
+              content: Text("Nama atau Alamat lokasi telah terdaftar"),
+            );})
+            ;
+          } else {
+            Navigator.pop(context);
+          }
+        }
+      },
+      style: ElevatedButton.styleFrom(
+          primary: Colors.indigo,
+          textStyle: const TextStyle(
+            color: Colors.white,
+          )));
+}
